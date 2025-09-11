3e49173dd1e4c6e0f9788bdb6c178d615427d807
Diegoww12a <diegoneves2021123@gmail.com>
2025-09-11 19:50:27 -0300

    atualizando site

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,50 @@
-// src/App.jsx
+import React from 'react';
+
 function App() {
+  // Coloque aqui todos os nomes dos arquivos
+  const avatars = [
+    'ALBARA.png',
+    'panicz.png',
+    'joao.png',
+    'maria.png',
+    'lucas.png',
+    'ana.png',
+    'bruno.png',
+    'carla.png',
+    'diego.png',
+    'elisa.png',
+    'felipe.png',
+    'gabi.png',
+    'henrique.png',
+    'isabela.png',
+    'julia.png',
+    'karen.png',
+    'leonardo.png',
+    'marcos.png',
+    'natalia.png',
+    'otavio.png',
+    'patricia.png',
+    'rafael.png',
+    'samuel.png',
+    'thais.png',
+    'vitoria.png',
+    'wesley.png',
+    'yasmin.png',
+    'zeca.png',
+    'tati.png',
+    'luana.png'
+  ];
+
   return (
     <div>
-      {/* Imagem franca.jpg que está em public/ */}
-      <img src="/franca.jpg" alt="França" width="300" />
-
-      {/* Outra imagem (logo.png) em public/ */}
-      <img src="/OVOTZ.png" alt="ovotz" width="150" />
-
+      <h1>Lista de Avatares</h1>
+      {avatars.map((avatar, index) => (
+        <img
+          key={index}
+          src={`/images/avatars/${avatar}`}
+          alt={`Avatar ${index + 1}`}
+        />
+      ))}
     </div>
   );
 }
