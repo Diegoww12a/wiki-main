32b99635ac1a13f6aaa0be92ea07616ee9e48f9e
Diego <diegoneves2021123@gmail.com>
2026-01-06 22:34:09 -0300

    Ultimo teste

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/wiki-main/', // <-- nome exato do repositório
+  base: './', // paths relativos para Netlify
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
